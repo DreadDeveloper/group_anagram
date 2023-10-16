18cd42be485366b3c737143ee5f36441da1f12af
Abhinav Kumar <kumaraka2000@gmail.com>
2023-10-16 18:14:04 +0530

    Initial Commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Solution solution = new Solution();
+//      solution.groupAnagrams(/*Send any list of lists of strings.*/);
+    }
+}
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> map = new HashMap<>();
+        for(String str : strs) {
+            char[] charArray = str.toCharArray();
+            Arrays.sort(charArray);
+            String sorted = new String(charArray);
+            if(!map.containsKey(sorted)) {
+                map.put(sorted, new LinkedList<String>());
+            }
+            map.get(sorted).add(str);
+        }
+        return new LinkedList<>(map.values());
     }
 }
